28ce1ed1b41455cc0628f4d6e306a65e8de00ce4
gabriellehora <gabrielle_hora@estudante.sesisenai.org.br>
2026-06-19 17:02:00 -0300

    Delete SA - zenstudy/SA - zenstudy directory

--- SA - zenstudy/SA - zenstudy/views/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../config/conexao.php';
-session_clear(); // Limpa as sessões
-session_destroy();
-
-session_start(); // Reinicia para conseguir mandar o flash
-flash('Você saiu do sistema.', 'info');
-redirecionar('/zenstudy/views/login.php');
-?>
